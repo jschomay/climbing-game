5aaa35470845b1ad5ae44f246aac87ecd2c3e6ad
Jeff Schomay <jschomay@gmail.com>
2016-12-02 17:41:57 -0700

    configure for heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var buildWall = require('./buildwall');
 
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static('./'));
 
@@ -60,8 +61,8 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(app.get('port'), function(){
+  console.log('listening on port ' + app.get('port'))
 });
 
 function generateRaceId() {
